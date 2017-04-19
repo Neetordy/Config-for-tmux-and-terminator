3c5c46915dd768b06bb86c6fedaf7164186bec7c
Neetordy <jokecreater@gmail.com>
2017-04-19 08:58:43 +0800

    Repair Script bugs

--- Autostart.sh
+++ Autostart.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 #
 # tumx启动脚本
 # mushan 2016-05-20
@@ -8,19 +8,19 @@ export DISABLE_AUTO_TITLE="true"
 
 session="Hatsune Miku"
 
-tmux has-session -t $session
+tmux has-session -t "$session"
 if [ $? = 0  ];then
-    tmux attach-session -t $session
+    tmux attach-session -t "$session"
     exit
 fi
 
-tmux new-session -d -s $session -n home
-tmux send-keys -t $session:1 'cd ~' C-m
-tmux new-window -t $session:2 -n edit vi
-tmux new-window -t $session:3 -n zsh
-tmux new-window -t $session:4 -n SSH 
-tmux send-keys -t $sesstion:4 'ssh root@' C-e C-m
+tmux new-session -d -s "$session" -n home
+tmux send-keys -t "$session":1 'cd ~' C-m
+tmux new-window -t "$session":2 -n edit vi
+tmux new-window -t "$session":3 -n zsh
+tmux new-window -t "$session":4 -n SSH
+tmux send-keys -t "$sesstion":4 'ssh root@' C-e C-m
 
-tmux split-window -t $session:3 -h
-tmux select-window -t $session:1
-tmux -2 attach-session -t $session
+tmux split-window -t "$session":3 -h
+tmux select-window -t "$session":1
+tmux -2 attach-session -t "$session"
